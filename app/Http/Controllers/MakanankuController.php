51d34668ebb2f31680b665e9e98a45d47ee4be8a
Wellson <wellsonleewando0908@gmail.com>
2019-12-11 23:25:38 +0700

    commit1

---
+++ app/Http/Controllers/MakanankuController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\makananku;
+use Illuminate\Http\Request;
+
+class MakanankuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\makananku  $makananku
+     * @return \Illuminate\Http\Response
+     */
+    public function show(makananku $makananku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\makananku  $makananku
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(makananku $makananku)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\makananku  $makananku
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, makananku $makananku)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\makananku  $makananku
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(makananku $makananku)
+    {
+        //
+    }
+}
---
+++ database/factories/MakanankuFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\makananku;
+use Faker\Generator as Faker;
+
+$factory->define(makananku::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
